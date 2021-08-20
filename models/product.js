0c059fae5d69867bb064700cd515c18868751f1b
gaurav dhra <dhradhra@gmail.com>
2021-08-20 18:07:07 +0530

    heal server

---
+++ models/product.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      maxlength: 32,
+      text: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    description: {
+      type: String,
+      maxlength: 2000,
+      text: true,
+    },
+    content: [
+      {
+        path: {
+          type: String,
+        },
+        title: {
+          type: String,
+        }
+      },
+    ],
+    price: {
+      type: Number,
+      trim: true,
+      maxlength: 32,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+    },
+    subs: [
+      {
+        type: ObjectId,
+        ref: "Sub",
+      },
+    ],
+    quantity: Number,
+    sold: {
+      type: Number,
+      required:false,
+      default: 0,
+    },
+    images: {
+      type: Array,
+    },
+    shipping: {
+      type: String,
+      enum: ["Yes", "No"],
+    },
+    color: {
+      type: String,
+      required: false,
+      enum: ["Black", "Brown", "Green", "Red", "Blue"],
+    },
+    brand: {
+      type: String,
+      required: false,
+      enum: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS"],
+    },
+    ratings: [
+      {
+        star: Number,
+        postedBy: { type: ObjectId, ref: "User" },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
